b85acf7a3f2918b12a295dd6e836a7c6219e5c93
Vynvan <68636193+Vynvan@users.noreply.github.com>
2024-10-22 15:57:44 +0200

    Implemented service of html files, static resources and template rendering

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
 const app = express();
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+console.log(dirname)
+console.log(path.join(dirname, 'assets'))
+
+// Statische Assets bereitstellen
+app.use('/assets', express.static(path.join(dirname, 'assets')));
+app.use('/css', express.static(path.join(dirname, 'css')));
+app.use('/theme/css', express.static(path.join(dirname, 'theme', 'css')));
+
 
 // EJS als View-Engine einrichten
 app.set("view engine", "ejs");
 
-// Route mit dynamischem Inhalt
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Todo EJS",
-    message: "Willkommen bei Ihrem ersten Express-Server!",
-  });
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index', { currentPage: 'index' });
 });
 
-// Weitere Route
-app.get("/about", (req, res) => {
-  res.send("Dies ist die About-Seite.");
+app.get('/todo-list', (req, res) => {
+  res.render('todo-list', { currentPage: 'todo-list' });
 });
+
+app.get('/praesentation', (req, res) => {
+  res.render('praesentation', { currentPage: 'presentation' });
+});
+
 
 // Server starten
 app.listen(3000, () => {
